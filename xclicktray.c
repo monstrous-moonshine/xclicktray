52eb39094eb44e2feeda979b7a366cd63d4a2630
Khalid Bin Walid <kbwalid26@gmail.com>
2023-06-04 23:34:32 +0900

    add debug messages

--- xclicktray.c
+++ xclicktray.c
@@ -75,43 +75,61 @@ int main(int argc, char *argv[])
     Atom _NET_SYSTEM_TRAY_S0 = XInternAtom(dpy, "_NET_SYSTEM_TRAY_S0", False);
     Window tray = XGetSelectionOwner(dpy, _NET_SYSTEM_TRAY_S0);
     if (!tray) {
-        fprintf(stderr, "System tray not found\n");
+        fprintf(stderr, "Could not find system tray\n");
         return 1;
     }
+#ifdef DEBUG
+    printf("System tray window: 0x%lX\n", tray);
+#endif
 
     int white_color = WhitePixel(dpy, DefaultScreen(dpy));
     Window w = XCreateSimpleWindow(dpy, DefaultRootWindow(dpy), 0, 0,
-                                   200, 100, 0, white_color, white_color);
-    GC gc = XCreateGC(dpy, w, 0, NULL);
-    XSetForeground(dpy, gc, white_color);
-    XSelectInput(dpy, w, ButtonPressMask | ExposureMask);
+                                   100, 100, 0, white_color, white_color);
+    XSelectInput(dpy, w, StructureNotifyMask | ButtonPressMask | ExposureMask);
+#ifdef DEBUG
+    printf("Created window: 0x%lX\n", w);
+#endif
+
+    // Embed into systray
     Atom _XEMBED_INFO = XInternAtom(dpy, "_XEMBED_INFO", False);
     const unsigned long xembed_info[2] = { XEMBED_VERSION, XEMBED_MAPPED };
     XChangeProperty(dpy, w, _XEMBED_INFO, _XEMBED_INFO, 32, PropModeReplace, (const unsigned char *)xembed_info, 2);
     send_message(dpy, tray, SYSTEM_TRAY_REQUEST_DOCK, w, 0, 0);
 
+    GC gc = XCreateGC(dpy, w, 0, NULL);
+    XSetForeground(dpy, gc, white_color);
+
     int is_open = 1;
     while (is_open) {
         XEvent ev;
-        XExposeEvent *expose_ev;
-        XButtonEvent *button_ev;
-
         XNextEvent(dpy, &ev);
 #ifdef DEBUG
         printf("Event received: %s\n", event_names[ev.type]);
 #endif
 
         switch (ev.type) {
+#ifdef DEBUG
+        case ClientMessage:
+            printf("Message type: %s\n", XGetAtomName(dpy, ev.xclient.message_type));
+            break;
+        case ConfigureNotify:
+            printf("%dx%d+%d+%d\n", ev.xconfigure.width, ev.xconfigure.height,
+                   ev.xconfigure.x, ev.xconfigure.y);
+            break;
+        case MapNotify:
+            break;
+        case ReparentNotify:
+            printf("New parent: 0x%lX\n", ev.xreparent.parent);
+            break;
+#endif
         case Expose:
-            expose_ev = &ev.xexpose;
-            XFillRectangle(dpy, w, gc, expose_ev->x, expose_ev->y,
-                           expose_ev->width, expose_ev->height);
+            XFillRectangle(dpy, w, gc, ev.xexpose.x, ev.xexpose.y,
+                           ev.xexpose.width, ev.xexpose.height);
             XSync(dpy, False);
             break;
         case ButtonPress:
-            button_ev = &ev.xbutton;
-            if (button_ev->button == Button1 || button_ev->button == Button3) {
-                if (button_ev->state & ShiftMask)
+            if (ev.xbutton.button == Button1 || ev.xbutton.button == Button3) {
+                if (ev.xbutton.state & ShiftMask)
                     is_open = 0;
                 else
                     spawn(&argv[1]);
